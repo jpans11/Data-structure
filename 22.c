a045f62d20d0629b211c912c863d8801e2875efd
jpans11 <165637127+jpans11@users.noreply.github.com>
2024-04-13 20:36:09 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -6,17 +6,17 @@
 int queue[MAX_SIZE];
 int front = -1, rear = -1;
 
-// Function to check if the queue is empty
+
 int isEmpty() {
     return (front == -1 && rear == -1);
 }
 
-// Function to check if the queue is full
+
 int isFull() {
     return (rear == MAX_SIZE - 1);
 }
 
-// Function to add an element to the queue
+
 void enqueue(int value) {
     if (isFull()) {
         printf("Queue is full\n");
@@ -29,7 +29,7 @@ void enqueue(int value) {
     queue[rear] = value;
 }
 
-// Function to remove an element from the queue
+
 int dequeue() {
     int deletedElement;
     if (isEmpty()) {
@@ -45,7 +45,6 @@ int dequeue() {
     return deletedElement;
 }
 
-// Function to display the elements of the queue
 void displayQueue() {
     if (isEmpty()) {
         printf("Queue is empty\n");
@@ -59,20 +58,20 @@ void displayQueue() {
 }
 
 int main() {
-    // Adding elements to the queue
+    
     enqueue(10);
     enqueue(20);
     enqueue(30);
 
-    // Displaying the queue before deleting an element
+    
     printf("Before deletion: ");
     displayQueue();
 
-    // Deleting an element from the queue
+    
     int deletedElement = dequeue();
     printf("Deleted element: %d\n", deletedElement);
 
-    // Displaying the queue after deleting an element
+    
     printf("After deletion: ");
     displayQueue();
 
