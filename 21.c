bfe4e3e4f88a6ace4e8dcc56b0df396bbf91a58c
jpans11 <165637127+jpans11@users.noreply.github.com>
2024-04-13 20:37:39 +0530

    Update 21.c

--- 21.c
+++ 21.c
@@ -4,17 +4,17 @@
 int front = -1, rear = -1;
 int queue[SIZE];
 
-// Function to check if the queue is full
+
 int isFull() {
     return rear == SIZE - 1;
 }
 
-// Function to check if the queue is empty
+
 int isEmpty() {
     return front == -1 || front > rear;
 }
 
-// Function to insert an element into the queue
+
 void enqueue(int value) {
     if (isFull()) {
         printf("Queue is full. Cannot insert element.\n");
@@ -26,7 +26,7 @@ void enqueue(int value) {
     printf("Element %d \n", value);
 }
 
-// Function to display the elements of the queue
+
 void display() {
     if (isEmpty()) {
         printf("Queue is empty. No elements to display.\n");
@@ -40,13 +40,13 @@ void display() {
 }
 
 int main() {
-    // Insert elements into the queue
+    
     enqueue(10);
     enqueue(20);
     enqueue(30);
     enqueue(40);
 
-    // Display elements of the queue
+    
     display();
 
     return 0;
